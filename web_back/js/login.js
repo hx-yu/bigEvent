c4f2157f2b8598ddf61ee3159c9c46acad173683
yuhexin <yuhexin@git.com>
2019-11-20 15:00:43 +0800

    第三次提交 完成用户登录页面弹窗修改

--- web_back/js/login.js
+++ web_back/js/login.js
@@ -2,12 +2,18 @@
 $('.input_sub').click(function() {
     var userName = $('.input_txt').val().trim();
     var userPassword = $('.input_pass').val().trim();
+    if (userName == '' || userPassword == '') {
+        $('#warning').text('用户名或密码不能为空');
+        $('#myModal').modal('show');
+        return;
+    }
     $.post('http://localhost:8000/admin/login', { user_name: userName, password: userPassword }, function(res) {
         console.log(res);
         if (res.code == 200) {
             location.href = './index.html';
         } else if (res.code == 400) {
-            alert(res.msg);
+            $('#warning').text(res.msg);
+            $('#myModal').modal('show');
         }
     })
 });
